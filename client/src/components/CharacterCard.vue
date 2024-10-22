4a94e63aea01398a990a29deecab63bbc2eac04a
jo <josehehall98@gmail.com>
2024-10-21 18:14:00 -0600

    push

--- client/src/components/CharacterCard.vue
+++ client/src/components/CharacterCard.vue
@@ -11,10 +11,10 @@ function setActiveCharacter(characterId) {
 </script>
 
 <template>
-  <div v-if="characterProp" class="card bg-primary">
+  <div v-if="characterProp" class="card bg-primary mb-2">
     <img @click="setActiveCharacter(characterProp.id)" :src="characterProp.image" :alt="characterProp.image">
-    <div class="d-flex justify-content-center">
-      <p class=" fw-semibold fs-5">{{ characterProp.name }}</p>
+    <div class="d-flex justify-content-center card-img-overlay align-items-end">
+      <p class="fw-semibold text-light mb-1  fs-5">{{ characterProp.name }}</p>
     </div>
   </div>
 </template>
@@ -24,5 +24,8 @@ img {
   height: 100%;
   object-fit: cover;
   box-shadow: 1px 1px 5px black
+}
+p{
+text-shadow: 1px 1px 2px rgb(0, 0, 0);
 }
 </style>
